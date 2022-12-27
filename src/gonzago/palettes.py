f44155235c7e4fdf44af4777a74f7587423ec7be
David Krummenacher <dave@kruda.ch>
2022-12-27 11:54:59 +0100

    Some minor changes to color class.

--- src/gonzago/palettes.py
+++ src/gonzago/palettes.py
@@ -4,13 +4,11 @@ import re
 
 
 @dataclass
-class Color:
+class Color(int):
     """Class that provides color value representation and functionality."""
 
-    value: int = 0
-
     def __str__(self):
-        return hex(self.value)
+        return hex(self)
 
     @staticmethod
     def from_string(color: str):
@@ -18,7 +16,9 @@ class Color:
         # if rgb:
         # TODO:
         # tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
-        return Color(int(color.lstrip("#").lower(), 16))
+
+        c: Color = int.__new__(Color, int(color.lstrip("#").lower(), 16))
+        return c
 
     def to_rgb(self) -> str:
-        return f"#{self.value:06x}"
+        return f"#{self:06x}"
