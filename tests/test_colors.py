ce63a04869148c74f25a357797ec2965bed5d664
David Krummenacher <dave@kruda.ch>
2022-12-31 00:02:20 +0100

    Made simpler. Starting from scratch.

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -1,7 +1,5 @@
 from gonzago.colors import Color
 
+
 def test_color():
-    test_color: str = "#123123"
-    color: Color = Color.create(test_color)
-    rgb: str = color.to_hex_rgb()
-    assert rgb == test_color
+    assert True
