ee0f2450253546f05b20311ed4c9557d9ef880d0
David Krummenacher <dave@kruda.ch>
2022-12-30 18:48:54 +0100

    Merp. Make simpler and treat palette as list of colors, ignore variants for now (can create methods that return a list of palettes).

--- src/gonzago/utils/colors.py
+++ src/gonzago/utils/colors.py
@@ -1,8 +1,30 @@
 from collections.abc import Iterable
 from typing import NamedTuple
+from abc import ABC, abstractmethod
+
+
+class RGBA(NamedTuple):
+    r: float = 0.0
+    g: float = 0.0
+    b: float = 0.0
+    a: float = 1.0
+
+
+class IColor(ABC):
+    @abstractmethod
+    def to_rgba(self) -> RGBA:
+        pass
+
+    @abstractmethod
+    @classmethod
+    def from_rgba(cls, rgba: RGBA):
+        pass
+
 
 class Color:
-    def __init__(self, r: float = 0.0, g: float = 0.0, b: float = 0.0, a: float = 1.0) -> None:
+    def __init__(
+        self, r: float = 0.0, g: float = 0.0, b: float = 0.0, a: float = 1.0
+    ) -> None:
         self.r = r
         self.g = g
         self.b = b
@@ -16,3 +38,5 @@ class Color:
 
     def __eq__(self, __o: object) -> bool:
         pass
+
+# https://refactoring.guru/design-patterns/python
