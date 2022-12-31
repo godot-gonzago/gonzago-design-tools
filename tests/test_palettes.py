edb41f7fad6a2320a82dacc3472808545f4068b1
David Krummenacher <dave@kruda.ch>
2022-12-31 23:34:07 +0100

    Merp.

--- tests/test_palettes.py
+++ tests/test_palettes.py
@@ -1,9 +1,9 @@
+from pathlib import Path
 import gonzago.palettes
 import tomlkit
 
 
 def test_download(capsys):
-    for palette in gonzago.palettes.download_color_pairs():
-        with capsys.disabled():
-            print(gonzago.palettes.convert_to_toml(palette).as_string())
+    with capsys.disabled():
+        print(gonzago.palettes.find_templates(Path('D:/Users/Dave/Documents/Godot/GonzagoFramework/GonzagoDesignTools/src/gonzago')))
 
