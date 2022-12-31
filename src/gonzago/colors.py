6455a9451df3353f8eaca52fb3d2c6e91b64c838
David Krummenacher <dave@kruda.ch>
2022-12-31 03:09:53 +0100

    Merp.

--- src/gonzago/colors.py
+++ src/gonzago/colors.py
@@ -1,7 +1,8 @@
 from abc import ABC
 from dataclasses import dataclass
+from math import sqrt
 from typing import NamedTuple
-from gonzago.math import RangedValue
+from gonzago.math import clamp01
 
 # https://stackoverflow.com/a/70700159
 # https://docs.python.org/3/howto/descriptor.html#validator-class
@@ -13,6 +14,15 @@ class Color(NamedTuple):
     g: float = 0.0
     b: float = 0.0
     a: float = 1.0
+
+    def clamped(self):
+        return Color(clamp01(self.r), clamp01(self.g), clamp01(self.b), clamp01(self.a))
+
+    def get_length(self, include_alpha: bool = False) -> float:
+        sum: float = self.r**2 + self.g**2 + self.b**2
+        if include_alpha:
+            sum += self.a**2
+        return sqrt(sum)
 
 
 # https://stackoverflow.com/questions/55973284/how-to-create-self-registering-factory-in-python
