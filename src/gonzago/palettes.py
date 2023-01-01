412213ed29e342da71e22a32e254d2c2499e8b2a
David Krummenacher <dave@kruda.ch>
2023-01-01 03:54:54 +0100

    Merp.

--- src/gonzago/palettes.py
+++ src/gonzago/palettes.py
@@ -1,6 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Generator, NamedTuple
+import re
 
 
 class Color(NamedTuple):
@@ -10,43 +11,61 @@ class Color(NamedTuple):
     a: float = 1.0
 
 
-class ColorInfo(NamedTuple):
-    name: str
-    description: str | None = None
-
-
-class PaletteInfo(NamedTuple):
-    file_name: str
-    name: str
-    description: str | None = None
-
-
 @dataclass
 class PaletteTemplate:
-    pass
+    themes: list[str] = field(default_factory=dict)
+    comment: str | None = None
+    version: str | None = None
 
 
 @dataclass
-class Palette:
-    pass
+class PaletteEntry:
+    name: str
+    color: Color
+    description: str | None = None
+    comment: str | None = None
+
+
+@dataclass
+class Palette(list[PaletteEntry]):
+    name: str
+    description: str | None = None
+    comment: str | None = None
+    # entries: list[PaletteEntry] = field(default_factory=list)
 
 
 # Find template files in folder (relative paths)
 def find_templates(template_folder: Path) -> list[Path]:
     # templates: list[Path] = []
-    # for path in template_folder.rglob('*.py'):
+    # for path in template_folder.rglob('*.txt'):
     #    templates.append(path.relative_to(input))
     # return templates
-    return list(template_folder.rglob("*.py"))
+    return list(template_folder.rglob("*.txt"))
 
 
 # Read template from file
 def read_template(template_path: Path) -> PaletteTemplate:
-    pass
+    # https://pythex.org/
+    # https://docs.python.org/3/howto/regex.html
+    comment_pattern = re.compile(
+        r'"(#[a-fA-F0-9]{6})".*"(#[a-fA-F0-9]{6})"(?:.*[\/]{2}?[ \t]*(\w+[ \t\w]*))?'
+    )
+
+    with template_path.open('r') as f:
+        # File header
+        m = re.match(r'^//\s*GonzagoDesignToolsPalette(?:\s+v[ersion]?=(?P<version>1.0.0)\b|$)?', f.readline())
+        if not m:
+            raise TypeError(f'File {template_path.absolute()} is not a valid palette template file!')
+
+        # comment = r'//\s*(?P<comment>\S.*)?'
+        # themes = r'//\s*@themes:\s*(?P<themes>\w+(?:\s*,\s*\w+)?)'
+        # name = r'^//\s*@name:\s*(?P<name>[^//|\n]*)(?:$|\s*//\s*(?P<comment>.*)$)' -> not working yet
+        # colors = (?P<colors>#[a-fA-F0-9]{6}(?:[ ]+#[a-fA-F0-9]{6})?)[ ]+(?P<name>[^//|\n]+)(?:[ ]*//[ ]*(?P<comment>.*))?
+        # color match (for use in colors) = r'(?P<color>#[a-fA-F0-9]{6})'
 
 
 # Generate palettes (exportable data structures from template) list from template
-def generate_palettes(template: PaletteTemplate):# -> Generator[Palette]:
+def generate_palettes(template: PaletteTemplate):  # -> Generator[Palette]:
     pass
 
 
