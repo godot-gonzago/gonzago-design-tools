f631a4fe6ddc093e41aaf0e513aef7882de06dd5
David Krummenacher <dave@kruda.ch>
2023-01-02 02:50:18 +0100

    Merp.

--- src/gonzago/__init__.py
+++ src/gonzago/__init__.py
@@ -1,2 +1,7 @@
 __version__ = "0.0.1"
 __all__ = ["palettes"]
+
+from pathlib import Path
+
+
+ROOT_PATH: Path = Path(__file__).parent.resolve()
--- tests/test_palettes.py
+++ tests/test_palettes.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from jsonschema import Draft202012Validator, validate
+from gonzago import ROOT_PATH
 
 import yaml
 import gonzago.palettes
@@ -10,11 +11,11 @@ def test_download(capsys):
     with capsys.disabled():
         print(gonzago.palettes.find_templates(Path(__file__, '../../src/gonzago')))
 
-    palette_path = Path(__file__, '../../src/gonzago/test.pal.yaml')
+    palette_path = Path(__file__, ROOT_PATH.joinpath('test.pal.yaml'))
     with palette_path.open() as f:
         palette = yaml.safe_load(f)
 
-    schema_path = Path(__file__, '../../src/gonzago/schema.pal.yaml')
+    schema_path = Path(__file__, ROOT_PATH.joinpath('schema.pal.yaml'))
     with schema_path.open() as f:
         schema = yaml.safe_load(f)
 
