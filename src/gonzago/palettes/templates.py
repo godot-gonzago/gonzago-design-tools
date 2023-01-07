5288ede939356dea477bed4fad4b822a7e942b5c
David Krummenacher <dave@kruda.ch>
2023-01-07 23:35:35 +0100

    Merp.

--- src/gonzago/palettes/templates.py
+++ src/gonzago/palettes/templates.py
@@ -6,12 +6,12 @@ from jsonschema import ValidationError, validate
 
 def get_valid_templates(template_folder: Path):
     # Load palette schema
-    schema_path: Path = Path(__file__).parent.joinpath('palettes.schema.yaml')
+    schema_path: Path = Path(__file__).parent.joinpath("palettes.schema.yaml")
     with schema_path.open() as schema_file:
         schema: dict = yaml.safe_load(schema_file)
 
     # Find valid templates in input folder
-    for template_path in template_folder.rglob('*.pal.yaml'):
+    for template_path in template_folder.rglob("*.pal.yaml"):
         with template_path.open() as template_file:
             template: dict = yaml.safe_load(template_file)
             try:
