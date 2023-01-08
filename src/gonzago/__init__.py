d8851fd854c7556d51e62a888792e133f1e4b14a
David Krummenacher <dave@kruda.ch>
2023-01-08 16:17:03 +0100

    Merp.

--- src/gonzago/__init__.py
+++ src/gonzago/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.0.1"
-__all__ = ["palettes"]
+__all__ = ["colors", "palettes"]
 
 from pathlib import Path
 
--- src/gonzago/colors/__init__.py
+++ src/gonzago/colors/__init__.py
@@ -1,1 +1,6 @@
 # https://inkscape.gitlab.io/inkscape/doxygen-extensions/colors_8py_source.html
+# https://github.com/meodai/color-names
+# https://en.wikipedia.org/wiki/Web_colors#Extended_colors
+# https://developer.mozilla.org/en-US/docs/Web/CSS/color_value
+# https://developer.mozilla.org/en-US/docs/Web/CSS/named-color
+# https://github.com/ubernostrum/webcolors
