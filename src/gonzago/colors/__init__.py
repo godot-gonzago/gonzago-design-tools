bbfb8e3b6b8bafdc8e95ed4e80ce09077ae399ea
David Krummenacher <dave@kruda.ch>
2023-01-08 18:52:09 +0100

    Merp.

--- src/gonzago/colors/__init__.py
+++ src/gonzago/colors/__init__.py
@@ -4,3 +4,18 @@
 # https://developer.mozilla.org/en-US/docs/Web/CSS/color_value
 # https://developer.mozilla.org/en-US/docs/Web/CSS/named-color
 # https://github.com/ubernostrum/webcolors
+
+class Color:
+    def __init__(
+        self,
+        name: str,
+        description: str | None = None,
+        r: int = 0,
+        g: int = 0,
+        b: int = 0,
+    ) -> None:
+        self.name = name
+        self.description = description
+        self.r = r
+        self.g = g
+        self.b = b
