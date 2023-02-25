0746b92a995ba339a6b5f91c3b2489be23c2a03c
David Krummenacher <dave@kruda.ch>
2023-02-25 14:26:35 +0100

    Merp.

--- src/gonzago/palettes/formats/paint_net.py
+++ src/gonzago/palettes/formats/paint_net.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+#from gonzago.palettes.io import PaletteWriterReader
 
-class PaintNetExporter:
+
+class PaintNetExporter: #(PaletteWriterReader):
     def export(self, template_data: dict):
         print("Paint.Net")
 
