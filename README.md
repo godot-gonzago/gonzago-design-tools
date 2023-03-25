eaa46fea4f95f3b7c7da2e035bd8e406c471bf86
David Krummenacher <dave@kruda.ch>
2023-03-25 22:26:38 +0100

    Still need to have Inkscape png export. Image effects in splash screen are not being exportet.

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Tools for Gonzago design assets.
 
 <https://python-poetry.org/docs/#installation>
 
+Windows User download <https://www.msys2.org/> and prepend System Variable Path with C:\msys64\usr\bin and C:\msys64\mingw64\bin.
+Install <https://packages.msys2.org/package/mingw-w64-x86_64-cairo>. Restart System and enjoy a working CairoSVG.
+
 ## Tools
 
 - <https://www.blender.org/>
--- tests/test_images.py
+++ tests/test_images.py
@@ -16,3 +16,7 @@ def test_svg_to_png(capsys):
         src_dir: Path = SCRIPT_PATH.joinpath("data/icon.svg")
         out_dir: Path = SCRIPT_PATH.joinpath("output/icon.png")
         gonzago.images.svg_to_png(src_dir, out_dir)
+
+        src_dir: Path = SCRIPT_PATH.joinpath("data/splash.svg")
+        out_dir: Path = SCRIPT_PATH.joinpath("output/splash.png")
+        gonzago.images.svg_to_png(src_dir, out_dir)
