d071c1c890d040509b9eafec3944886adcdf51f5
David Krummenacher <dave@kruda.ch>
2023-03-25 09:43:27 +0100

    Added gitignore for test output.

--- tests/test_images.py
+++ tests/test_images.py
@@ -8,5 +8,5 @@ SCRIPT_PATH: Path = Path(__file__).parent.resolve()
 def test_icons(capsys):
     with capsys.disabled():
         src_dir: Path = SCRIPT_PATH.joinpath("data")
-        out_dir: Path = SCRIPT_PATH.joinpath("out")
+        out_dir: Path = SCRIPT_PATH.joinpath("output")
         gonzago.images.optimize_icons(src_dir, out_dir)
