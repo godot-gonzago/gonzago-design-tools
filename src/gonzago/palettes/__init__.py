d49b00436456d6e86e9d7784ec131f9093cee5d3
David Krummenacher <dave@kruda.ch>
2023-04-02 00:14:57 +0200

    Back to basics. Fuck that complex shit.

--- src/gonzago/palettes/__init__.py
+++ src/gonzago/palettes/__init__.py
@@ -11,80 +11,18 @@ from .io import PaletteReader, PaletteWriter, PaletteWriterReader
 __all__ = ["Palette", "Color", "PaletteReader", "PaletteWriter", "PaletteWriterReader"]
 
 
-# https://inkscape.gitlab.io/inkscape/doxygen-extensions/colors_8py_source.html
-# class Color:
-#    def __init__(
-#        self,
-#        name: str,
-#        description: str | None = None,
-#        r: int = 0,
-#        g: int = 0,
-#        b: int = 0,
-#    ) -> None:
-#        self.name = name
-#        self.description = description
-#        self.r = r
-#        self.g = g
-#        self.b = b
-#
-#
-# class Palette:
-#    def __init__(
-#        self,
-#        name: str,
-#        description: str | None = None,
-#        version: str | None = None,
-#        source: str | None = None,
-#    ) -> None:
-#        self.name = name
-#        self.description = description
-#        self.version = version
-#        self.source = source
-#        self.colors = list[Color]
-#
-#
-# class PaletteTemplate:
-#    pass
-#
-#
-# def get_valid_templates(template_folder: Path):
-#    # Load palette schema
-#    schema_path: Path = Path(__file__).parent.joinpath("palettes.schema.yaml")
-#    with schema_path.open() as schema_file:
-#        schema: dict = yaml.safe_load(schema_file)
-#
-#    # Find valid templates in input folder
-#    for template_path in template_folder.rglob("*.pal.yaml"):
-#        with template_path.open() as template_file:
-#            template: dict = yaml.safe_load(template_file)
-#            try:
-#                validate(template, schema)
-#                yield template_path, template
-#            except ValidationError:
-#                pass
-#            except:
-#                pass
+EXPORTERS = dict()
 
 
-def build_palettes_from_templates(template_folder: Path, output_folder: Path) -> None:
-    #    from gonzago.palettes.templates import get_valid_templates
-    #
-    #    for template_path, template in get_valid_templates(template_folder):
-    #        rel_path = template_path.relative_to(template_folder)
-    #        print(rel_path)
+def exporter(suffix: str):
+    def inner(fn):
+        EXPORTERS[suffix] = fn
+        return fn
 
-    # find templates in input folder
-    # for each template
-    #   load template
-    #   generate palettes from template
-    #   for each palette
-    #       for each exporter
-    #           generate output path from template path and palette info? this might be made easier
-    #           export template to output path
-    pass
+    return inner
 
 
-def build_templates(src_dir: Path, out_dir: Path):
+def build_palettes(src_dir: Path, out_dir: Path):
     import yaml
     from jsonschema import validate
 
@@ -113,34 +51,118 @@ def build_templates(src_dir: Path, out_dir: Path):
         print("No valid palette templates found.")
         return
 
-    # Write palette
+    # Call exporters
     for rel_path in templates:
-        out_file: Path = out_dir.joinpath(rel_path).with_suffix(".gpl").resolve()
-        out_file.parent.mkdir(parents=True, exist_ok=True)  # Ensure folders
-        print(out_file)
-
         template = templates[rel_path]
+        for suffix in EXPORTERS:
+            out_file: Path = out_dir.joinpath(rel_path).with_suffix(suffix).resolve()
+            out_file.parent.mkdir(parents=True, exist_ok=True)  # Ensure folders
+            print(out_file)
+            EXPORTERS[suffix](out_file, template)
 
-        with out_file.open("w") as file:
-            file.write("GIMP Palette\n")
-            file.write(f"Name: {template['name']}\n")
-            file.write(f"Columns: 0\n")
 
-            if template.get("description", None):
-                file.write(f"# Description: {template['description']}\n")
-            if template.get("version", None):
-                file.write(f"# Version: {template['version']}\n")
-            if template.get("author", None):
-                file.write(f"# Author: {template['author']}\n")
-            if template.get("source", None):
-                file.write(f"# Source: {template['source']}\n")
+@exporter(".png")
+def export_png(out_file: Path, template: dict, size: int = 1):
+    from PIL import Image, ImageDraw
 
-            file.write(f"#")
+    color_count: int = len(template["colors"])
+    image: Image = Image.new("RGB", (color_count * size, size))
 
-            for color_data in template["colors"]:
-                file.write("\n")
-                for i in [1,3,5]:
-                    file.write(f"{str(int(color_data['color'][i:i+2], 16))}\t")
-                file.write(color_data["name"])
-                if color_data.get("description", None):
-                    file.write(f" - {color_data['description']}")
+    draw = ImageDraw.Draw(image, "RGB")
+    for i in range(color_count):
+        color = template["colors"][i]["color"]
+        draw.rectangle((i * size, 0, i * size + size, size), color)
+
+    image.save(out_file, "PNG")
+
+
+@exporter(".gpl")
+def export_gimp(out_file: Path, template: dict):
+    with out_file.open("w") as file:
+        file.write("GIMP Palette\n")
+        file.write(f"Name: {template['name']}\n")
+        file.write(f"Columns: 0\n")
+
+        if template.get("description", None):
+            file.write(f"# Description: {template['description']}\n")
+        if template.get("version", None):
+            file.write(f"# Version: {template['version']}\n")
+        if template.get("author", None):
+            file.write(f"# Author: {template['author']}\n")
+        if template.get("source", None):
+            file.write(f"# Source: {template['source']}\n")
+
+        file.write(f"#")
+
+        for color_data in template["colors"]:
+            file.write("\n")
+            for i in [1, 3, 5]:
+                file.write(f"{str(int(color_data['color'][i:i+2], 16))}\t")
+            file.write(color_data["name"])
+            if color_data.get("description", None):
+                file.write(f" - {color_data['description']}")
+
+
+@exporter(".hex")
+def export_hex(out_file: Path, template: dict):
+    colors = [c["color"].lstrip("#").lower() for c in template["colors"]]
+    with out_file.open("w") as file:
+        file.writelines("\n".join(colors))
+
+
+# @exporter(".ase")
+# def export_adobe_swatch_exchange(out_file: Path, template: dict):
+#    # https://medium.com/swlh/mastering-adobe-color-file-formats-d29e43fde8eb
+#    with out_file.open("wb") as file:
+#        # Write header
+#        file.write(b"\x41\x53\x45\x46")  # Signature (Constant: ASEF)
+#        file.write(b"\x00\x01\x00\x00")  # Version (Constant: 1.0)
+#        file.write(b"\x00\x00\x00\x03")  # Number of blocks
+#
+#        # Group start
+#        file.write(b"\xC0\x01")  # Block type (Group start)
+#        file.write(b"\x00\x00\x00\x00")  # Block length
+#        file.write(b"\x00\x00")  # Name length
+#        file.write(b"\x00")  # 0-terminated group name encoded in UTF-16
+#
+#        # Color entry
+#        file.write(b"\x00\x01")  # Block type (Color entry)
+#        file.write(b"\x00\x00\x00\x00")  # Block length
+#        file.write(b"\x00\x00")  # Name length
+#        file.write(b"\x00")  # 0-terminated color name encoded in UTF-16
+#        file.write(b"\x52\x47\x42\x20")  # Color space (Constant: RGB)
+#        file.write(b"\x00\x00\x00\x00")  # Red
+#        file.write(b"\x00\x00\x00\x00")  # Green
+#        file.write(b"\x00\x00\x00\x00")  # Blue
+#        file.write(b"\x00\x02")  # Color mode (Constant: Normal)
+#
+#        # Group end
+#        file.write(b"\xC0\x02")  # Block type (Group end)
+#        file.write(b"\x00\x00\x00\x00")  # Block length (Constant for Group end)
+
+
+# @exporter(".txt")
+# def export_paint_net(out_file: Path, template: dict):
+#    https://www.getpaint.net/doc/latest/WorkingWithPalettes.html
+#    ;paint.net Palette File
+#    ;Downloaded from Lospec.com/palette-list
+#    ;Palette Name: Lospec500
+#    ;Description: A collaboration from the Lospec Discord server to create a palette celebrating 500 palettes hosted on Lospec.
+#    ;Colors: 42
+#    FF10121c
+#    FF2c1e31
+#    FF6b2643
+#    pass
+
+
+# @exporter(".txt")
+# def export_jasc(out_file: Path, template: dict):
+#    # https://liero.nl/lierohack/docformats/other-jasc.html
+#    # JASC-PAL      <- constant string
+#    # 0100          <- constant version of palette file format
+#    # 16            <- color count
+#    # 255 0 0       <- [0-255] rgb separated by space
+#    # 0 255 0
+#    # 0 0 255
+#    # 255 255 0
+#    pass
--- tests/test_palettes.py
+++ tests/test_palettes.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from gonzago.palettes import build_templates
+from gonzago.palettes import build_palettes
 
 from gonzago.palettes.io import find_palette_file_handlers, get_valid_templates
 
@@ -13,5 +13,5 @@ def test_io(capsys):
 
         #find_palette_file_handlers()
 
-        build_templates(SCRIPT_PATH.joinpath("data"), SCRIPT_PATH.joinpath("output"))
+        build_palettes(SCRIPT_PATH.joinpath("data"), SCRIPT_PATH.joinpath("output"))
         pass
