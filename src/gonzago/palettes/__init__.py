6d296b0e0fcca855d860622af3555cc41c1f4185
David Krummenacher <dave@kruda.ch>
2023-04-01 22:36:16 +0200

    Simplified palettes export. Can be simplified more.

--- src/gonzago/palettes/__init__.py
+++ src/gonzago/palettes/__init__.py
@@ -12,7 +12,7 @@ __all__ = ["Palette", "Color", "PaletteReader", "PaletteWriter", "PaletteWriterR
 
 
 # https://inkscape.gitlab.io/inkscape/doxygen-extensions/colors_8py_source.html
-#class Color:
+# class Color:
 #    def __init__(
 #        self,
 #        name: str,
@@ -28,7 +28,7 @@ __all__ = ["Palette", "Color", "PaletteReader", "PaletteWriter", "PaletteWriterR
 #        self.b = b
 #
 #
-#class Palette:
+# class Palette:
 #    def __init__(
 #        self,
 #        name: str,
@@ -43,11 +43,11 @@ __all__ = ["Palette", "Color", "PaletteReader", "PaletteWriter", "PaletteWriterR
 #        self.colors = list[Color]
 #
 #
-#class PaletteTemplate:
+# class PaletteTemplate:
 #    pass
 #
 #
-#def get_valid_templates(template_folder: Path):
+# def get_valid_templates(template_folder: Path):
 #    # Load palette schema
 #    schema_path: Path = Path(__file__).parent.joinpath("palettes.schema.yaml")
 #    with schema_path.open() as schema_file:
@@ -67,11 +67,11 @@ __all__ = ["Palette", "Color", "PaletteReader", "PaletteWriter", "PaletteWriterR
 
 
 def build_palettes_from_templates(template_folder: Path, output_folder: Path) -> None:
-#    from gonzago.palettes.templates import get_valid_templates
-#
-#    for template_path, template in get_valid_templates(template_folder):
-#        rel_path = template_path.relative_to(template_folder)
-#        print(rel_path)
+    #    from gonzago.palettes.templates import get_valid_templates
+    #
+    #    for template_path, template in get_valid_templates(template_folder):
+    #        rel_path = template_path.relative_to(template_folder)
+    #        print(rel_path)
 
     # find templates in input folder
     # for each template
@@ -82,3 +82,57 @@ def build_palettes_from_templates(template_folder: Path, output_folder: Path) ->
     #           generate output path from template path and palette info? this might be made easier
     #           export template to output path
     pass
+
+
+def build_templates(src_dir: Path, out_dir: Path):
+    import yaml
+    from jsonschema import validate
+
+    # Load palette schema
+    schema_path: Path = Path(__file__).parent.joinpath("palettes.schema.yaml")
+    with schema_path.open() as schema_file:
+        schema: dict = yaml.safe_load(schema_file)
+
+    # Find valid templates in input folder
+    templates: dict = dict()
+    for src_file in src_dir.rglob("*.pal.y[a]ml"):
+        with src_file.open() as template_file:
+            template: dict = yaml.safe_load(template_file)
+            try:
+                validate(template, schema)
+            except:
+                continue
+            rel_path: Path = src_file.with_suffix("").relative_to(src_dir)
+            templates[rel_path] = template
+
+    print(templates)
+
+    # Write palette
+    for rel_path in templates.keys():
+        out_file: Path = out_dir.joinpath(rel_path).with_suffix(".gpl").resolve()
+        out_file.parent.mkdir(parents=True, exist_ok=True)  # Ensure folders
+        print(out_file)
+
+        with out_file.open("w") as file:
+            file.write("GIMP Palette\n")
+            file.write(f"Name: {template['name']}\n")
+            file.write(f"Columns: 0\n")
+
+            if template.get("description", None):
+                file.write(f"# Description: {template['description']}\n")
+            if template.get("version", None):
+                file.write(f"# Version: {template['version']}\n")
+            if template.get("author", None):
+                file.write(f"# Author: {template['author']}\n")
+            if template.get("source", None):
+                file.write(f"# Source: {template['source']}\n")
+
+            file.write(f"#")
+
+            for color_data in template["colors"]:
+                file.write("\n")
+                for i in [1,2,5]:
+                    file.write(f"{str(int(color_data['color'][i:i+2], 16))}\t")
+                file.write(color_data["name"])
+                if color_data.get("description", None):
+                    file.write(f" - {color_data['description']}")
--- tests/test_palettes.py
+++ tests/test_palettes.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from gonzago.palettes import build_templates
 
 from gonzago.palettes.io import find_palette_file_handlers, get_valid_templates
 
@@ -7,8 +8,10 @@ SCRIPT_PATH: Path = Path(__file__).parent.resolve()
 
 def test_io(capsys):
     with capsys.disabled():
-        for template in get_valid_templates(SCRIPT_PATH):
-            print(template)
+        #for template in get_valid_templates(SCRIPT_PATH):
+        #    print(template)
 
-        find_palette_file_handlers()
+        #find_palette_file_handlers()
+
+        build_templates(SCRIPT_PATH.joinpath("data"), SCRIPT_PATH.joinpath("output"))
         pass
