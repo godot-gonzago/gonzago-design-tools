d11caa4efe7c0336bf7e70196d81a541025e0346
David Krummenacher <dave@kruda.ch>
2023-04-02 09:50:37 +0200

    Merp.

--- src/gonzago/videos/__init__.py
+++ src/gonzago/videos/__init__.py
@@ -1,1 +1,5 @@
 # https://docs.blender.org/manual/en/latest/advanced/command_line/render.html
+# https://wiki.blender.org/wiki/Building_Blender/Other/BlenderAsPyModule
+# https://docs.blender.org/manual/en/latest/files/media/video_formats.html
+# https://github.com/kkroening/ffmpeg-python
+# https://github.com/jonghwanhyeon/python-ffmpeg
