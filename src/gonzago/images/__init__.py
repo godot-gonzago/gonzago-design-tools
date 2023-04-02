7e5dc89ba4dad52bf305accfab245daf0e6091d0
David Krummenacher <dave@kruda.ch>
2023-04-02 14:00:43 +0200

    Some reorganisation and started logic for os icons. Need to implement scaled svg to png export first.

--- src/gonzago/images/__init__.py
+++ src/gonzago/images/__init__.py
@@ -52,7 +52,9 @@ def inkscape_to_png(src_file: Path, out_file: Path) -> None:
     # https://docs.python.org/3/library/subprocess.html#subprocess.run
 
     inkscape: Path = Path("C:/Program Files/Inkscape/bin/inkscape.exe").resolve()
-    blender: Path = Path("C:/Program Files/Blender Foundation/Blender 3.4/blender.exe").resolve()
+    blender: Path = Path(
+        "C:/Program Files/Blender Foundation/Blender 3.4/blender.exe"
+    ).resolve()
 
     # Sanitize paths
     src_file: Path = src_file.resolve(True)
@@ -77,13 +79,82 @@ def svg_to_png(src_file: Path, out_file: Path) -> None:
 
     # Convert (https://cairosvg.org/documentation/)
     from cairosvg import svg2png
+
     svg2png(url=str(src_file), write_to=str(out_file))
 
 
-def build_os_icons() -> None:
-    # ICO https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#ico
-    # ICNS https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#icns
+def build_os_icons(src_files: set[Path], out_dir: Path) -> None:
+    import xml.etree.ElementTree as ET
+
+    # ICO
+    # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#ico
+    # https://docs.godotengine.org/en/stable/tutorials/export/changing_application_icon_for_windows.html
+    # https://en.wikipedia.org/wiki/ICO_(file_format)
+    # https://www.geeksforgeeks.org/convert-png-to-ico-with-pillow-in-python/
+    # https://learning-python.com/iconify.html
+    # https://github.com/python-pillow/Pillow/pull/6122
+    # 16×16,    24x24, 32×32,    48×48, 64×64, 128×128,    256×256
+
+    # ICNS
+    # https://docs.fileformat.com/image/icns/
+    # https://en.wikipedia.org/wiki/Apple_Icon_Image_format
+    # https://pypi.org/project/icnsutil/
+    # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#icns
+    # 16×16,           32×32,    48×48,        128×128,    256×256,    512x512,
+    # 16x16@2x,        32x32@2x,               128x128@2x, 256x256@2x, 512x512@2x
+
+    # TODO: Check sizes of source files
+    src_file_size_dict: dict = dict()
+    for src_file in src_files:
+        if src_file.suffix != ".svg":
+            continue
+        src_file = src_file.resolve(True)
+        tree = ET.parse(src_file)
+        root = tree.getroot()
+        src_file_size: int = max(
+            int(root.attrib.get("width", "0")), int(root.attrib.get("height", "0"))
+        )
+        if src_file_size == 0:
+            continue
+        src_file_size_dict[src_file_size] = src_file
+
+    # TODO: Temporary folder export pngs, get source files based on requested size >= source size
+    src_file_size_list: list[int] = sorted(src_file_size_dict.keys(), reverse=True)
+
+    print(src_file_size_dict)
+    print(src_file_size_list)
+
+    # 16×16 + @2x
+    # 24x24
+    # 32×32 + @2x
+    # 48×48
+    # 64×64
+    # 128×128 + @2x
+    # 256×256 + @2x
+    # 512x512 + @2x
+
+    target_sizes: list[int] = [16, 24, 32, 48, 64, 128, 256, 512]
+    target_scales: list[int] = [16, 32, 128, 256, 512]
+
+    src_file_size: int = -1
+    for target_size in target_sizes:
+        # Find source files based on requested size >= source size
+        for src_file_size_entry in src_file_size_list:
+            if (
+                src_file_size_entry <= target_size
+                and src_file_size_entry > src_file_size
+            ):
+                src_file_size = src_file_size_entry
+
+        print(
+            f"target_size:{target_size} -> src_size:{src_file_size}, plus scaling: {target_size in target_scales} -> {src_file_size_dict[src_file_size]}"
+        )
+
     # MOBILE?
+
+    # Ensure folders
+    # out_dir = out_dir.resolve()
+    # out_dir.mkdir(parents=True, exist_ok=True)
     pass
 
 
--- tests/test_images.py
+++ tests/test_images.py
@@ -7,17 +7,24 @@ SCRIPT_PATH: Path = Path(__file__).parent.resolve()
 
 def test_icons(capsys):
     with capsys.disabled():
-        src_dir: Path = SCRIPT_PATH.joinpath("data")
-        out_dir: Path = SCRIPT_PATH.joinpath("output")
+        src_dir: Path = SCRIPT_PATH.joinpath("data/icons")
+        out_dir: Path = SCRIPT_PATH.joinpath("output/icons")
         gonzago.images.optimize_icons(src_dir, out_dir)
 
 
 def test_svg_to_png(capsys):
     with capsys.disabled():
-        src_dir: Path = SCRIPT_PATH.joinpath("data/icon.svg")
-        out_dir: Path = SCRIPT_PATH.joinpath("output/icon.png")
+        src_dir: Path = SCRIPT_PATH.joinpath("data/icons/icon.svg")
+        out_dir: Path = SCRIPT_PATH.joinpath("output/png/icon.png")
         gonzago.images.svg_to_png(src_dir, out_dir)
 
         src_dir: Path = SCRIPT_PATH.joinpath("data/splash.svg")
-        out_dir: Path = SCRIPT_PATH.joinpath("output/splash.png")
+        out_dir: Path = SCRIPT_PATH.joinpath("output/png/splash.png")
         gonzago.images.inkscape_to_png(src_dir, out_dir)
+
+
+def test_os_icons(capsys):
+    with capsys.disabled():
+        src_dir: Path = SCRIPT_PATH.joinpath("data/application")
+        out_dir: Path = SCRIPT_PATH.joinpath("output/application")
+        gonzago.images.build_os_icons(src_dir.glob("*.svg"), out_dir)
