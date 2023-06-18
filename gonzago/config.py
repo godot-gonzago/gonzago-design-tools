98b36745db8e7a7a1142bb3bf87f36964dcb9932
David Krummenacher <dave@kruda.ch>
2023-06-18 11:13:23 +0200

    Removed some more stuff.

--- gonzago/config.py
+++
@@ -1,5 +0,0 @@
-# https://www.geeksforgeeks.org/python-os-environ-object/
-# https://www.programcreek.com/python/?code=mahart-studio%2Fkivystudio%2Fkivystudio-master%2Fkivystudio%2Ftools%2F__init__.py
-
-# Default value based on git repo? https://github.com/acroz/git-find-repos/blob/master/git_find_repos.py
-# In .git folder in config file > [remote "origin"] > url = https://github.com/godot-gonzago/gonzago-design-tools.git
